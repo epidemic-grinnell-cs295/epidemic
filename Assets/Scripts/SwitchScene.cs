75351c171bc76999463ba5cf9ad679fc7463fd79
Leonora Bresette Buccino <norabuccino@Leonoras-MacBook-Pro.local>
2016-04-06 14:27:37 -0500

    added location scenes and back buttons

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour {
+
+	public string scene;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+
+		SceneManager.LoadScene (scene);
+	}
+}
