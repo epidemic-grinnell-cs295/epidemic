d28b9efebef1b054f13cbaf049c3ddcff00e65f1
Leonora Bresette Buccino <norabuccino@amber-laptop.grinnell.edu>
2016-04-10 14:40:04 -0500

    added checkboxes and fixed clock text

---
+++ Assets/Scripts/ClockDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ClockDisplay : MonoBehaviour {
+
+	public Text clockText;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
