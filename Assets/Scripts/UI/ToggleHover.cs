5d91c267bb54f78012b622d62acfddfd517009b1
mulhalle <mulhalle@grinnell.edu>
2016-05-08 16:49:49 -0500

    fix bug with popup text

--- Assets/Scripts/UI/ToggleHover.cs
+++ Assets/Scripts/UI/ToggleHover.cs
@@ -7,8 +7,6 @@ public class ToggleHover : MonoBehaviour {
 	public GameObject newSprite;
 	private Vector3 currentSpritePosition;
 	public Text text;
-	public Text hideText;
-	public GameObject hideSprite;
 	public string popupText;
 
 	// Use this for initialization
@@ -25,14 +23,10 @@ public class ToggleHover : MonoBehaviour {
 		Debug.Log ("Hovering over now");
 		newSprite.GetComponent<Renderer>().enabled = true;
 		text.text = popupText;
-		hideText.enabled = false;
-		hideSprite.GetComponent<Renderer> ().enabled = false;
 	}
 	public void Exit () {
 		Debug.Log ("Not hovering");
 		newSprite.GetComponent<Renderer> ().enabled = false;
 		text.text = "";
-		hideText.enabled = true;
-		hideSprite.GetComponent<Renderer> ().enabled = true;
 	}
 }
